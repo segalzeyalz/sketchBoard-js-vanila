93841ef8ade15c5710b34f22f7236fe628c98543
segalzeyalz <segaleseo@gmail.com>
2017-12-19 10:58:22 +0200

    moving it to js 6

--- js/actToolbar.js
+++ js/actToolbar.js
@@ -1,44 +1,43 @@
-window.onload = function(){
+window.onload = () =>{
 
     //Define all the elements of the toolbar
 
-    var rectBtn = document.getElementById("rectBtn");
+    let rectBtn = document.getElementById("rectBtn");
     rectBtn.addEventListener("click", randRect);
 
-    var ovalBtn = document.getElementById("ovalBtn");
+    let ovalBtn = document.getElementById("ovalBtn");
     ovalBtn.addEventListener("click", randOval);
 
-    var colorBtn = document.getElementById("colorBtn");
+    let colorBtn = document.getElementById("colorBtn");
     colorBtn.addEventListener("change", changeColor);
 
 
-    var deleteBtn = document.getElementById("deleteBtn");
+    let deleteBtn = document.getElementById("deleteBtn");
     deleteBtn.addEventListener("click", removeShape);
     document.addEventListener("keydown", removeShapeByDelete);
 
     function removeShapeByDelete(e){
     if (!e) e = window.event;
-    var keyCode = e.keyCode || e.which;
+    let keyCode = e.keyCode || e.which;
     if (keyCode == '46'){
       removeShape(e);
     }
   }
 
 
-  var canvas = document.getElementById("canvas");
+  let canvas = document.getElementById("canvas");
  // canvas.addEventListener("click", changeSelect);
   
-    var closeLoad = document.getElementById("closeLoad");
+    let closeLoad = document.getElementById("closeLoad");
     closeLoad.addEventListener("click", closeWindow);
     closeLoad.addEventListener("keypress", closeWindowByEsc);
 
-    var LoadBtn = document.getElementById("LoadBtn");
+    let LoadBtn = document.getElementById("LoadBtn");
     LoadBtn.addEventListener("click", openLoadModal);
 
-    var SaveBtn = document.getElementById("SaveBtn");
+    let SaveBtn = document.getElementById("SaveBtn");
     SaveBtn.addEventListener("click", openSaveModal);
 
     		// When the user clicks on <span> (x), close the modal
-
     
 }
